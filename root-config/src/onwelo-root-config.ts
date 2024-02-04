96928b1c0f6ca288856ea72ad2ef0ecadf496326
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2024-02-04 20:24:15 +0100

    FIX VUE PROBLEM

--- root-config/src/onwelo-root-config.ts
+++ root-config/src/onwelo-root-config.ts
@@ -1,39 +1,39 @@
-import { registerApplication, start } from 'single-spa';
+import { registerApplication, start } from "single-spa";
 
 registerApplication(
-  '@onwelo/events-list',
-  //@ts-ignore
-  () => System.import('@onwelo/events-list'),
-  // The function below is the "activity function", which must be provided
-  (location) => location.pathname.startsWith('/events-list')
+    "@onwelo/events-list",
+    //@ts-ignore
+    () => System.import("@onwelo/events-list"),
+    // The function below is the "activity function", which must be provided
+    (location) => location.pathname.startsWith("/events-list")
 );
 
 registerApplication(
-  'add-event',
-  //@ts-ignore
-  () => System.import('add-event'),
-  // The function below is the "activity function", which must be provided
-  (location) => location.pathname.startsWith('/add-event')
+    "add-event",
+    //@ts-ignore
+    () => System.import("add-event"),
+    // The function below is the "activity function", which must be provided
+    (location) => location.pathname.startsWith("/add-event")
 );
 
 registerApplication(
-  '@onwelo/home-page',
-  //@ts-ignore
-  () => System.import('@onwelo/home-page'),
-  // The function below is the "activity function", which must be provided
-  (location) => location.pathname.startsWith('/home')
+    "@onwelo/home-page",
+    //@ts-ignore
+    () => System.import("@onwelo/home-page"),
+    // The function below is the "activity function", which must be provided
+    (location) => location.pathname.startsWith("/home")
 );
 
 start({
-  urlRerouteOnly: true,
+    urlRerouteOnly: true,
 });
 function loadWithoutAmd(name) {
-  return Promise.resolve().then(() => {
-    let globalDefine = (window as any).define;
-    delete (window as any).define;
-    return System.import(name).then((module) => {
-      (window as any).define = globalDefine;
-      return module;
+    return Promise.resolve().then(() => {
+        let globalDefine = (window as any).define;
+        delete (window as any).define;
+        return System.import(name).then((module) => {
+            (window as any).define = globalDefine;
+            return module;
+        });
     });
-  });
 }
