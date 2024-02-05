c40443371485ab7b65e5060e490ca00cb8e51fb9
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2024-02-05 21:06:57 +0100

    FIX LAYOUT

--- root-config/src/onwelo-root-config.ts
+++ root-config/src/onwelo-root-config.ts
@@ -1,29 +1,43 @@
 import { registerApplication, start } from "single-spa";
+import { constructApplications, constructLayoutEngine, constructRoutes } from "single-spa-layout";
+import micro from "./microfrontend-layout.html";
+// registerApplication(
+//     "@onwelo/events-list",
+//     //@ts-ignore
+//     () => System.import("@onwelo/events-list"),
+//     // The function below is the "activity function", which must be provided
+//     (location) => location.pathname.startsWith("/events-list")
+// );
 
-registerApplication(
-    "@onwelo/events-list",
-    //@ts-ignore
-    () => System.import("@onwelo/events-list"),
-    // The function below is the "activity function", which must be provided
-    (location) => location.pathname.startsWith("/events-list")
-);
+// registerApplication(
+//     "add-event",
+//     //@ts-ignore
+//     () => System.import("add-event"),
+//     // The function below is the "activity function", which must be provided
+//     (location) => location.pathname.startsWith("/add-event")
+// );
 
-registerApplication(
-    "add-event",
-    //@ts-ignore
-    () => System.import("add-event"),
-    // The function below is the "activity function", which must be provided
-    (location) => location.pathname.startsWith("/add-event")
-);
+// registerApplication(
+//     "@onwelo/home-page",
+//     //@ts-ignore
+//     () => System.import("@onwelo/home-page"),
+//     // The function below is the "activity function", which must be provided
+//     (location) => location.pathname.startsWith("/home")
+// );
 
-registerApplication(
-    "@onwelo/home-page",
-    //@ts-ignore
-    () => System.import("@onwelo/home-page"),
-    // The function below is the "activity function", which must be provided
-    (location) => location.pathname.startsWith("/home")
-);
+const routes = constructRoutes(micro);
+const applications = constructApplications({
+    routes,
+    loadApp({ name }) {
+        return System.import(name);
+    },
+});
 
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+
+layoutEngine.activate();
 start({
     urlRerouteOnly: true,
 });
